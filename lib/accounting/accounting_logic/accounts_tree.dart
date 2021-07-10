339fbd17bed69f440a33286f5c40e322892f0d8e
saeed yaghuti <sarbaztanha707@gmail.com>
2021-07-10 11:08:30 +0400

    add perm-requirement to AcountModle

--- lib/accounting/accounting_logic/accounts_tree.dart
+++ lib/accounting/accounting_logic/accounts_tree.dart
@@ -17,6 +17,8 @@ const ACCOUNTS_TREE = const [
     readTransactionPermissionsAny: [
       PermissionModel.LEDGER_READ_ALL_TRANSACTION
     ],
+    editTransactionPermissionsAny: [],
+    deleteTransactionPermissionsAny: [],
   ),
   AccountModel(
     id: ACCOUNTS_ID.SALES_ACCOUNT_ID,
@@ -25,6 +27,12 @@ const ACCOUNTS_TREE = const [
     titleArabic: 'مبيعات',
     titlePersian: 'فروش',
     note: '',
+    createTransactionPermissionsAny: [
+      PermissionModel.SALES_CREATE_TRANSACTION_X
+    ],
+    readTransactionPermissionsAny: [PermissionModel.SALES_READ_ALL_TRANSACTION],
+    editTransactionPermissionsAny: [],
+    deleteTransactionPermissionsAny: [],
   ),
   AccountModel(
     id: ACCOUNTS_ID.EXPENDITURE_ACCOUNT_ID,
@@ -33,6 +41,21 @@ const ACCOUNTS_TREE = const [
     titleArabic: 'مصاريف',
     titlePersian: 'هزینه ها',
     note: '',
+    createTransactionPermissionsAny: [
+      PermissionModel.EXPENDITURE_CREATE_TRANSACTION,
+    ],
+    readTransactionPermissionsAny: [
+      PermissionModel.EXPENDITURE_READ_ALL_TRANSACTION,
+      PermissionModel.EXPENDITURE_READ_OWN_TRANSACTION,
+    ],
+    editTransactionPermissionsAny: [
+      PermissionModel.EXPENDITURE_EDIT_ALL_TRANSACTION,
+      PermissionModel.EXPENDITURE_EDIT_OWN_TRANSACTION,
+    ],
+    deleteTransactionPermissionsAny: [
+      PermissionModel.EXPENDITURE_DELETE_ALL_TRANSACTION,
+      PermissionModel.EXPENDITURE_DELETE_OWN_TRANSACTION,
+    ],
   ),
   AccountModel(
     id: ACCOUNTS_ID.ASSETS_ACCOUNT_ID,
@@ -41,6 +64,14 @@ const ACCOUNTS_TREE = const [
     titleArabic: 'اصول',
     titlePersian: 'دارایی ها',
     note: '',
+    createTransactionPermissionsAny: [
+      PermissionModel.ASSETS_CREATE_TRANSACTION_X
+    ],
+    readTransactionPermissionsAny: [
+      PermissionModel.ASSETS_READ_ALL_TRANSACTION
+    ],
+    editTransactionPermissionsAny: [],
+    deleteTransactionPermissionsAny: [],
   ),
   AccountModel(
     id: ACCOUNTS_ID.BANKS_ACCOUNT_ID,
@@ -49,6 +80,12 @@ const ACCOUNTS_TREE = const [
     titleArabic: 'بنوك',
     titlePersian: 'بانک ها',
     note: '',
+    createTransactionPermissionsAny: [
+      PermissionModel.BANKS_CREATE_TRANSACTION_X
+    ],
+    readTransactionPermissionsAny: [PermissionModel.BANKS_READ_ALL_TRANSACTION],
+    editTransactionPermissionsAny: [],
+    deleteTransactionPermissionsAny: [],
   ),
   AccountModel(
     id: ACCOUNTS_ID.NBO_ACCOUNT_ID,
@@ -57,6 +94,21 @@ const ACCOUNTS_TREE = const [
     titleArabic: 'بنک عمان الوطني',
     titlePersian: 'بانک ان بی او',
     note: '',
+    createTransactionPermissionsAny: [
+      PermissionModel.NBO_CREATE_TRANSACTION,
+    ],
+    readTransactionPermissionsAny: [
+      PermissionModel.NBO_READ_ALL_TRANSACTION,
+      PermissionModel.NBO_READ_OWN_TRANSACTION,
+    ],
+    editTransactionPermissionsAny: [
+      PermissionModel.NBO_EDIT_ALL_TRANSACTION,
+      PermissionModel.NBO_EDIT_OWN_TRANSACTION,
+    ],
+    deleteTransactionPermissionsAny: [
+      PermissionModel.NBO_DELETE_ALL_TRANSACTION,
+      PermissionModel.NBO_DELETE_OWN_TRANSACTION,
+    ],
   ),
   AccountModel(
     id: ACCOUNTS_ID.PETTY_CASH_ACCOUNT_ID,
@@ -65,6 +117,21 @@ const ACCOUNTS_TREE = const [
     titleArabic: 'بتي كش',
     titlePersian: 'تن خواه',
     note: '',
+    createTransactionPermissionsAny: [
+      PermissionModel.PETTY_CASH_CREATE_TRANSACTION,
+    ],
+    readTransactionPermissionsAny: [
+      PermissionModel.PETTY_CASH_READ_ALL_TRANSACTION,
+      PermissionModel.PETTY_CASH_READ_OWN_TRANSACTION,
+    ],
+    editTransactionPermissionsAny: [
+      PermissionModel.PETTY_CASH_EDIT_ALL_TRANSACTION,
+      PermissionModel.PETTY_CASH_EDIT_OWN_TRANSACTION,
+    ],
+    deleteTransactionPermissionsAny: [
+      PermissionModel.PETTY_CASH_DELETE_ALL_TRANSACTION,
+      PermissionModel.PETTY_CASH_DELETE_OWN_TRANSACTION,
+    ],
   ),
   AccountModel(
     id: ACCOUNTS_ID.CASH_DRAWER_ACCOUNT_ID,
@@ -73,6 +140,21 @@ const ACCOUNTS_TREE = const [
     titleArabic: 'صندوق',
     titlePersian: 'صندوق',
     note: '',
+    createTransactionPermissionsAny: [
+      PermissionModel.CASH_DRAWER_CREATE_TRANSACTION,
+    ],
+    readTransactionPermissionsAny: [
+      PermissionModel.CASH_DRAWER_READ_ALL_TRANSACTION,
+      PermissionModel.CASH_DRAWER_READ_OWN_TRANSACTION,
+    ],
+    editTransactionPermissionsAny: [
+      PermissionModel.CASH_DRAWER_EDIT_ALL_TRANSACTION,
+      PermissionModel.CASH_DRAWER_EDIT_OWN_TRANSACTION,
+    ],
+    deleteTransactionPermissionsAny: [
+      PermissionModel.CASH_DRAWER_DELETE_ALL_TRANSACTION,
+      PermissionModel.CASH_DRAWER_DELETE_OWN_TRANSACTION,
+    ],
   ),
   AccountModel(
     id: ACCOUNTS_ID.DEBTORS_ACCOUNT_ID,
@@ -81,6 +163,14 @@ const ACCOUNTS_TREE = const [
     titleArabic: 'بنک عمان الوطني',
     titlePersian: 'المدينين',
     note: '',
+    createTransactionPermissionsAny: [
+      PermissionModel.DEBTORS_CREATE_TRANSACTION_X
+    ],
+    readTransactionPermissionsAny: [
+      PermissionModel.DEBTORS_READ_ALL_TRANSACTION
+    ],
+    editTransactionPermissionsAny: [],
+    deleteTransactionPermissionsAny: [],
   ),
   AccountModel(
     id: ACCOUNTS_ID.LIABILITIES_ACCOUNT_ID,
@@ -89,5 +179,13 @@ const ACCOUNTS_TREE = const [
     titleArabic: 'بنک عمان الوطني',
     titlePersian: 'الدين الحالي',
     note: '',
+    createTransactionPermissionsAny: [
+      PermissionModel.LIABILITIES_CREATE_TRANSACTION_X
+    ],
+    readTransactionPermissionsAny: [
+      PermissionModel.LIABILITIES_READ_ALL_TRANSACTION
+    ],
+    editTransactionPermissionsAny: [],
+    deleteTransactionPermissionsAny: [],
   ),
 ];
