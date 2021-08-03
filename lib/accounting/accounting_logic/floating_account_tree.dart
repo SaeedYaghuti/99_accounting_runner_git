bb9067b923575c8ee1f5479964bcfe629aae47b8
saeed yaghuti <sarbaztanha707@gmail.com>
2021-08-04 00:18:21 +0400

    doing float

--- lib/accounting/accounting_logic/floating_account_tree.dart
+++ lib/accounting/accounting_logic/floating_account_tree.dart
@@ -5,24 +5,50 @@ import 'package:shop/accounting/expenditure/expenditure_class_tree.dart';
 import '../accounting_logic/transaction_classification.dart';
 
 const FLOAT_ACCOUNT_TREE = const [
+  // Root
+  FloatingAccount(
+    id: FloatAccountIds.ROOT_FLOAT_ACCOUNT_ID,
+    parentId: FloatAccountIds.TOP_FLOAT_ACCOUNT_ID,
+    titleEnglish: 'Root Floating Account',
+    titlePersian: 'شناور ريشه',
+    titleArabic: 'سيال اساسي',
+    note: 'This is root float account for all other float account',
+  ),
   // GENERAL
   FloatingAccount(
-    id: TranClassIds.GENERAL_TRAN_CLASS_ID,
-    parentId: TranClassIds.TOP_TRAN_CLASS_ID,
-    titleEnglish: 'General Transaction Class',
-    titlePersian: 'طبقه عمومى',
-    titleArabic: 'تصنيف عامة',
-    note: 'Use this class when classification is not defined for account',
+    id: FloatAccountIds.GENERAL_FLOAT_ACCOUNT_ID,
+    parentId: FloatAccountIds.ROOT_FLOAT_ACCOUNT_ID,
+    titleEnglish: 'General Floating Account',
+    titlePersian: 'شناور عمومى',
+    titleArabic: 'سيال عامة',
+    note: 'Use this class when float account is not need for transaction',
   ),
-  // MAIN_EXPENDITURE
+  // SALESMAN
   FloatingAccount(
-    id: ExpClassIds.MAIN_EXP_CLASS_ID,
-    parentId: ExpClassIds.TOP_EXP_CLASS_ID,
-    accountType: ACCOUNTS_ID.EXPENDITURE_ACCOUNT_ID,
-    titleEnglish: 'Expenses Classifications',
-    titlePersian: 'طبقه بندى هزينه ها',
-    titleArabic: 'تصنيف مصاريف',
-    note: 'Parent of all expenditure classes',
+    id: FloatAccountIds.SALESMAN_FLOAT_ACCOUNT_ID,
+    parentId: FloatAccountIds.ROOT_FLOAT_ACCOUNT_ID,
+    titleEnglish: 'Salesman',
+    titlePersian: 'شناور فروشنده ها',
+    titleArabic: 'سيال بائعين',
+    note: 'Salesman floating account',
+  ),
+  // SAEID
+  FloatingAccount(
+    id: FloatAccountIds.SAEID_FLOAT_ACCOUNT_ID,
+    parentId: FloatAccountIds.SALESMAN_FLOAT_ACCOUNT_ID,
+    titleEnglish: 'Saeid',
+    titlePersian: 'شناور سعيد ',
+    titleArabic: 'سيال سعيد',
+    note: 'Saeid floating account',
+  ),
+  // SADI
+  FloatingAccount(
+    id: FloatAccountIds.SADI_FLOAT_ACCOUNT_ID,
+    parentId: FloatAccountIds.SALESMAN_FLOAT_ACCOUNT_ID,
+    titleEnglish: 'Sadi',
+    titlePersian: 'شناور سعدي ',
+    titleArabic: 'سيال سعدي',
+    note: 'Sadi floating account',
   ),
 ];
 
@@ -36,6 +62,7 @@ List<FloatingAccount> childs(String expClassId) {
 
 class FloatAccountIds {
   static const TOP_FLOAT_ACCOUNT_ID = 'TOP_FLOAT';
+  static const ROOT_FLOAT_ACCOUNT_ID = 'ROOT_FLOAT';
   static const GENERAL_FLOAT_ACCOUNT_ID = 'GENERAL_FLOAT';
   static const SALESMAN_FLOAT_ACCOUNT_ID = 'SALESMAN_FLOAT';
   static const SAEID_FLOAT_ACCOUNT_ID = 'SAEID_FLOAT';
