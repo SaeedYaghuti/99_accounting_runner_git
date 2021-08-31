7f23712d4567f0c2098a0f7ff660865298c4c419
saeed yaghuti <sarbaztanha707@gmail.com>
2021-09-01 00:40:10 +0400

    learn create simple custom from field

--- lib/shared/test_screen/test_screen.dart
+++ lib/shared/test_screen/test_screen.dart
@@ -11,24 +11,147 @@ class TestScreen extends StatefulWidget {
   _TestScreenState createState() => _TestScreenState();
 }
 
+// class _TestScreenState extends State<TestScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Text('Welcome to Test Screen!'),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {},
+//         tooltip: 'Save',
+//         child: Icon(
+//           Icons.check,
+//           size: 36.0,
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 class _TestScreenState extends State<TestScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  String? _name;
+  int? _age;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Center(
-        child: Text('Welcome to Test Screen!'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Save',
-        child: Icon(
-          Icons.check,
-          size: 36.0,
+        child: Form(
+          key: this._formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text('Please fill in your name and age'),
+                TextFormField(
+                  autovalidate: false,
+                  onSaved: (value) => this._name = value,
+                  validator: (value) {
+                    if (value == null || value.length < 3) {
+                      return 'a minimum of 3 characters is required';
+                    }
+                  },
+                ),
+                Counter(),
+                RaisedButton(
+                  child: Text('Submit'),
+                  onPressed: () {
+                    if (this._formKey.currentState!.validate()) {
+                      setState(() {
+                        this._formKey.currentState!.save();
+                      });
+                    }
+                  },
+                ),
+                SizedBox(height: 50.0),
+                Text('${this._name} is ${this._age} years old')
+              ],
+            ),
+          ),
         ),
       ),
     );
   }
+}
+
+class Counter extends StatefulWidget {
+  @override
+  _CounterState createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  late int value;
+
+  @override
+  void initState() {
+    super.initState();
+    this.value = 0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        IconButton(
+          icon: Icon(Icons.remove),
+          onPressed: () {
+            setState(() {
+              this.value--;
+            });
+          },
+        ),
+        Text(this.value.toString()),
+        IconButton(
+          icon: Icon(Icons.add),
+          onPressed: () {
+            setState(() {
+              this.value++;
+            });
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class CounterFormField extends FormField<int> {
+  CounterFormField(
+      {required FormFieldSetter<int> onSaved,
+      required FormFieldValidator<int> validator,
+      int initialValue = 0,
+      bool autovalidate = false})
+      : super(
+            onSaved: onSaved,
+            validator: validator,
+            initialValue: initialValue,
+            autovalidate: autovalidate,
+            builder: (FormFieldState<int> state) {
+              return Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.remove),
+                    onPressed: () {
+                      state.didChange(state.value - 1);
+                    },
+                  ),
+                  Text(state.value.toString()),
+                  IconButton(
+                    icon: Icon(Icons.add),
+                    onPressed: () {
+                      state.didChange(state.value + 1);
+                    },
+                  ),
+                ],
+              );
+            });
 }
